77b49d06a29734f6ca9e38f109ce9958ecd46705
Ben Allfree <ben@benallfree.com>
2017-12-04 15:35:39 -0800

    Source map update

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,6 @@ module.exports = {
     rules: [
       { test: /\.js$/, use: ['babel-loader'] }
     ]
-  }
+  },
+  devtool: 'source-map'
 }
--- webpack.config.testing.js
+++
@@ -1,5 +0,0 @@
-var config = require('./webpack.config.js')
-
-config.devtool = 'source-map'
-
-module.exports = config
